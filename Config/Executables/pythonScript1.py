b434175154a1c5866ba2949f12ee5186ee7b989e
Tatjana Tomic <tatjanatomic997@gmail.com>
2022-07-01 04:55:59 -0700

    Added Config and Server directories, updated App.config file

---
+++ Config/Executables/pythonScript1.py
@@ -0,0 +1,7 @@
+import time
+import sys
+
+print("pythonScript1.py started...")
+print('Entered parameters:', sys.argv)
+time.sleep(2.4)
+print("pythonScript1.py finished. Execution time 2.4s")
---
+++ Config/Executables/pythonScript2.py
@@ -0,0 +1,5 @@
+import time
+
+print("pythonScript2.py started...")
+time.sleep(2.8)
+print("pythonScript2.py finished. Execution time 2.8s")
